95d720e9d56d93e4341f024e42671ba1a0a7bb8f
Karamdeep Bassi <karamdeepbassi@gmail.com>
2024-01-15 16:56:03 -0500

    Home created

--- website-portfolio/src/App.js
+++ website-portfolio/src/App.js
@@ -2,12 +2,26 @@ import './App.css';
 
 function App() {
   return (
-    <div className = "App">
-      <header className = "Header">
-        <button className = "HeaderButton"><span>Home</span></button>
-        <button className = "HeaderButton"><span>About Me</span></button>
-        <button className = "HeaderButton"><span>Projects</span></button>
+    <div className="App">
+      <header className="Header" src="Headshot.png">
+        <button className="HeaderButton"><span>Home</span></button>
+        <button className="HeaderButton"><span>About Me</span></button>
+        <button className="HeaderButton"><span>Projects</span></button>
+        <button className="HeaderButton"><span>Contact</span></button>
       </header>
+      <div className="Home">
+        <article>
+          <p className = "HomeContent">
+            <h1>
+              Software Engineer
+            </h1>
+          </p>
+          <p className = "HomeContentDescription">
+            Hello! Im Karamdeep Bassi, a software engineer based in Maryland, USA!
+          </p>
+        </article>
+        <img className="Headshot" src="Images/Headshot.png" alt="Headshot"/>
+      </div>
     </div>
   );
 }
